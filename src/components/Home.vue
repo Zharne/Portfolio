88f58e165153a021fad782eb6c9f1e97aeaa197c
Zharne <zharnedesember@gmail.com>
2022-03-15 23:47:29 +0200

    changes

---
+++ src/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container"><br><br><br><br>
+        <div class='img-box'>
+            <img src="https://i.postimg.cc/Gp5rLhQK/me-shot-min.jpg" style="border-radius:50%">
+        </div><br>
+        <div class="text">
+             ZHARNÉ DESEMBER 
+        </div><br>
+        <div class="button">
+
+        </div>             
+    </div>    
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+.container{
+    background-color:  #111;
+    height: 100vh;
+    margin-left: 250px;
+    width: 80%;
+    border-radius:10px
+
+} 
+.img-box {
+	width: 200px;
+	height: 200px;
+	margin: 0 auto;
+	padding: 5px;
+	border: 1px solid #ddd;
+	border-radius: 50%;
+    
+}
+.img-box img {
+	width: 100%;
+	height: 100%;
+	display: block;
+	border-radius: 50%;
+    
+}
+
+/* 
+@keyframes img-box {
+  0%,
+  49% {
+    background: black;
+  }
+  50%,
+  100% {
+    background: white;
+  }
+} */
+
+</style>
